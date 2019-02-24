9a5b22ac9e964942c1be27297707a3c31b743173
lya <liuyaowa@gmail.com>
2019-02-24 15:39:19 +0800

    fix bug

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from video.settings import BASE
+from video.settings import BASE_DIR
 import re
 import os
 from wsgiref.util import FileWrapper
@@ -8,11 +8,12 @@ from .tasks import generate_cover
 from .models import VideoInfo
 from django.core.paginator import Paginator
 from django.conf import settings
+import shutil
 
 
 def add_video_path(request):
     if request.method == 'GET':
-        return render(request, 'no_video.html')
+        return render(request, 'no_video.html', {'delete': True})
     path = request.POST.get('path')
     print(path)
     add_path(path)
@@ -72,8 +73,7 @@ def file_iterator(file_name, chunk_size=8192, offset=0, length=None):
 
 def play(request, id):
     video = VideoInfo.objects.get(id=id)
-    p = video.video_path
-    path = os.path.join(BASE, p)
+    path = video.video_path
     """将视频文件以流媒体的方式响应"""
     range_header = request.META.get('HTTP_RANGE', '').strip()
     range_re = re.compile(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)', re.I)
@@ -82,7 +82,6 @@ def play(request, id):
     # content_type, encoding = mimetypes.guess_type(path)
     content_type = 'application/octet-stream'
     if range_match:
-        print(1)
         first_byte, last_byte = range_match.groups()
         first_byte = int(first_byte) if first_byte else 0
         last_byte = first_byte + 1024 * 1024 * 1  # 8M 每片,响应体最大体积
@@ -94,7 +93,6 @@ def play(request, id):
         resp['Content-Length'] = str(length)
         resp['Content-Range'] = 'bytes %s-%s/%s' % (first_byte, last_byte, size)
     else:
-        print(2)
         # 不是以视频流方式的获取时，以生成器方式返回整个文件，节省内存
         resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type='application/octet-stream')
         resp['Content-Length'] = str(size)
@@ -107,8 +105,20 @@ def upload(request):
         return render(request, 'upload.html')
     files = request.FILES['file']
     file_name = files.name
-    f = open(os.path.join(BASE, file_name), 'wb+')
+    if not os.path.exists(os.path.join(BASE_DIR, 'uploads')):
+        os.mkdir(os.path.join(BASE_DIR, 'uploads'))
+    path = os.path.join(BASE_DIR, 'uploads', file_name)
+    f = open(path, 'wb+')
     for file in files.chunks():
         f.write(file)
     f.close()
+    add_path(path)
+    generate_cover.delay(path)
     return render(request, 'alert.html', {'alert': '视频已添加，稍后就能展示'})
+
+
+def delete_videos(request):
+    shutil.rmtree(os.path.join(BASE_DIR, 'images'))
+    shutil.rmtree(os.path.join(BASE_DIR, 'uploads'))
+    VideoInfo.objects.all().delete()
+    return render(request, 'alert.html', {'alert': '视频库已删除(包括已上传的视频)，请重新添加'})
--- video/settings.py
+++ video/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-BASE = 'E:\source'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,7 +122,7 @@ STATICFILES_DIRS = [
 ]
 
 # 需要保存的视频的扩展名
-VIDEOS_EXT = ['.mp4', '.mkv', '.wmv']
+VIDEOS_EXT = ['.mp4']
 
 HAYSTACK_CONNECTIONS = {
     'default': {
